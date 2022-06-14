cfdfea66da9be94f78f464c687d4463ab48be9c2
Lenifc <lenifc@outlook.vom>
2022-06-14 17:18:52 +0200

    add endGame conditions, generate an apple

--- code.js
+++ code.js
@@ -2,20 +2,23 @@ const board = document.querySelector('#board')
 
 const snakeStart = [{x: 10, y: 10}]
 let snakePosition = snakeStart
+let applePosition = { x: Math.floor(Math.random()*20+1), y: Math.floor(Math.random()*20+1)} // generuje jabłko w losowym miejscu po wczytaniu gry
 let snakeSpeed = 1 // ilosć odświeżeń na sekundę
 let lastRefreshTime = 0
 let directions = {x: 0, y: 0}
 let lastMove = directions
+let gameEnd = false
 
 // requestAnimationFrame lepsza alternatywa dla setInterval
-function refreshAnimation(getCurrentTime){
-    requestAnimationFrame(refreshAnimation)
+refreshAnimation = (getCurrentTime) => {
+    if(!gameEnd) requestAnimationFrame(refreshAnimation)
 
     // zapobiega nadmiernemu odświeżaniu pozycji - ogranicza jedynie do nieco ponad jednej sekundy
     if((getCurrentTime - lastRefreshTime) / 1000 <= 1 / snakeSpeed) return
 
     updatePosition()
     renderSnake()
+    renderApple()
     lastRefreshTime = getCurrentTime
 }
 requestAnimationFrame(refreshAnimation)
@@ -29,9 +32,11 @@ function updatePosition(){
 
     // odświeżenie pozycji na podstawie ostatniego naciśniętego klawisza
     console.log(directions);
+    console.log(applePosition);
     lastMove = directions
     snakePosition[0].x += directions.x
     snakePosition[0].y += directions.y
+    if((snakePosition[0].x > 20 || snakePosition[0].x < 1 || snakePosition[0].y > 20 || snakePosition[0].y < 1)) gameEnd = true
 }
 
 // generuje kwadraty w odpowiednich miejscach na planszy
@@ -47,11 +52,21 @@ function renderSnake(){
         board.appendChild(snakeSquare)
     })
 }
+
+// generuje jeden kwadrat z jabłkiem w losowym miejscu na planszy wykluczając zajęte pozycje przez węża
+function renderApple(){
+    let appleSquare = document.createElement('div')
+    appleSquare.id = 'apple' // kazdy utworzony element otrzymuje ID, aby móc zaaplikować odpowiednie stylowanie
+
+    appleSquare.style.gridColumnStart = applePosition.x
+    appleSquare.style.gridRowStart = applePosition.y
+
+    board.appendChild(appleSquare)
+}
     
 // sprawdza ktory klawisz byl ostatnio wcisniety i nakierowuje węża w odpowiednią stronę
 // zawiera także zabezpieczenie, aby nie można wykonać nagłego zwrotu o 180stopni
 window.addEventListener('keydown', e => {
-    console.log(e.code);
     if(e.code == 'ArrowUp') directions = lastMove.y === 1 ? directions : {x: 0, y: -1 } // oś Y jest odwrócona
     if(e.code == 'ArrowDown') directions = lastMove.y === -1 ? directions : {x: 0, y: 1} // oś Y jest odwrócona
     if(e.code == 'ArrowRight') directions = lastMove.x === -1 ? directions : {x: 1, y: 0}
